78ad1e96f30111c004c6f82612afc0b1e754aa52
vivianzhou1288 <vivianzhou1288@gmail.com>
2022-06-05 12:50:45 -0400

    commit

---
+++ vite-project/js/array.js
@@ -0,0 +1,58 @@
+import "../styles/resume.css";
+
+const experience = [
+  {
+    name: "Salvation Army",
+    description: "Volunteer | Dec 2018 - Mar 2020",
+    img: "https://rkdgroup.com/wp-content/uploads/2020/04/the-salvation-army-1-logo-png-transparent.png",
+  },
+  {
+    name: "Luv Michael",
+    description: "Fundraising Volunteer | Feb 2021",
+    img: "https://res.cloudinary.com/dktp1ybbx/image/upload/f_auto,fl_lossy,q_auto/v1632554096/organization/prod/1183648/7UKyzYqDNc.png",
+  },
+  {
+    name: "Audubon Leadership Council",
+    description: "Council Member | Mar 2021 - Jun 2021",
+    img: "https://www.pngkey.com/png/full/980-9803436_audubon-society-national-audubon-society.png",
+  },
+  {
+    name: "SYEP (Summer Youth Employment Program)",
+    description: "Census Research Intern | Jul 2019 - Aug 2019",
+    img: "https://www1.nyc.gov/assets/dycd/images/content/header/logo.png",
+  },
+  {
+    name: "Girls x Math",
+    description: "Mentor | May 2021 - Aug 2021",
+    img: "https://images.squarespace-cdn.com/content/v1/605fef44c17ee2510405bbac/1616976602523-0ECPR16RDQIVBSQ26G07/Untitled_Artwork+17.png?format=1500w",
+  },
+  {
+    name: "PitchTeen",
+    description:
+      "Technology Manager | Aug 2021 - Present <br><br> Marketing Manager | Aug 2021 - Present",
+    img: "https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/challenge_thumbnails/001/472/590/datas/original.png",
+  },
+  {
+    name: "Clubhouse-to-Career/College (C2C) Pathways Program ",
+    description: "Member | Jan 2022 - Present",
+    img: "https://www.bklynlibrary.org/sites/default/files/styles/eventcal_large/public/images/events/calendar/BBY_TeenTechCenter_Lockup_Logo_vt_RGB.png",
+  },
+  {
+    name: "Brooklyn Public Library",
+    description:
+      "Technology Intern | Jul 2021 - Present <br><br> Book Buddy | Jul 2019 - Aug 2019",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Brooklyn_Public_Library_logo.svg/1280px-Brooklyn_Public_Library_logo.svg.png",
+  },
+  {
+    name: "Caddell Prep",
+    description: "Junior Web Developer Intern | Jul 2021 - Aug 2021",
+    img: "https://d19j0qt0x55bap.cloudfront.net/production/startups/5b8bfc86547a996a520000df/images/startup_image/square_BestCaddellPrepLogo_Blue_Square.jpg",
+  },
+  {
+    name: "STEM Without Boundaries",
+    description: "Director of Media | Jun 2021 - Present",
+    img: "https://static.wixstatic.com/media/b2004d_ef2ca99fbf4849bb927477dab21ca97e~mv2.png/v1/crop/x_0,y_83,w_619,h_455/fill/w_260,h_188,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/big%20logo%202_PNG.png",
+  },
+];
+export { experience };
+console.log(experience);
